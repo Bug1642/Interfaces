de356ab7b4fd6184b1d73fb10416474bef783a36
Bug1642 <hasancan1642@hotmail.com>
2024-12-31 15:44:55 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Exercise
+{
+    public interface Area
+    {
+        double GetArea();
+    }
+    public class Rectangle : Area
+    {
+        double height;
+        double width;
+
+        public Rectangle(double height, double width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+        public double GetArea()
+        {
+            return height * width;
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args) 
+        {
+            Rectangle rectangle = new Rectangle(5.0,5.0);
+            Console.WriteLine(rectangle.GetArea());
+        }
+    }
+}
